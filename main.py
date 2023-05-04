9b4b7830c1254fb098c534b77d6737489cf61359
jdicao <105899704+jdicao@users.noreply.github.com>
2023-05-03 20:53:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,30 @@ from typing import List, Optional
 
 app = FastAPI()
 
-class Persona (BaseModel):
+class Persona(BaseModel):
     id: int
     nombre: str
     edad: int
     ciudad: Optional[str] = None
 
-personaList = []
+listaPersonas = []
 
 @app.post("/personas", response_model=Persona)
 def crear_persona(person: Persona):
-    personaList.append(person)
+    listaPersonas.append(person)
     return person
 
 @app.get("/personas", response_model=List[Persona])
-def get_personas():
-    return personaList
+def obtener_personas():
+    return listaPersonas
 
-@app.get("/personas/{persona_id}", response_model=Persona)
-def obtener_persona (persona_id: int):
-    for persona in personaList:
+@app.get("/personas/{persona_id}",response_model=Persona)
+def buscar_persona(persona_id: int):
+    for persona in listaPersonas:
         if persona.id == persona_id:
             return persona
     raise HTTPException(status_code=404, detail="Persona no encontrada")
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Interoperabilidad"}
+    return {"Hello": "Interoperavilidad"}
